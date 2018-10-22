eee6020fe1a009a21897dd4ab52fcfc7f8aeeed8
Alejandro Ortiz <alejandro.ortiz@unosquare.com>
2018-10-22 17:07:57 -0500

    first

--- Multiplication/Program.cs
+++ Multiplication/Program.cs
@@ -7,6 +7,15 @@ namespace Multiplication
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Ingrese un numero");
+            int a = Int32.Parse(Console.ReadLine());
+
+            for(int i = 0; i<=10; i++)
+            {
+                Console.WriteLine($"{a} X {i} = {a*i}");
+            }
+            Console.ReadKey();
         }
     }
 }
